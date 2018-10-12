414235940d513d88505b4283062ec7a25cd6d075
kingchris1031 <alan_grant10@yahoo.com>
2018-10-12 12:48:21 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import random
+
+food=('Burger King','Five Guys','BGR Joint',' Nandos', 'Checkers', 'New Fortune', 'Subway', 'Wendys', 'Baja Fresh', 'Jerrys', 'Corner Bakery', 'Red Robin', 'McDonalds', 'KFC', 'Panera',' Taco Bar', 'IHop(IHob)', 'Popeyes', 'Red Lobster', 'Chipotle', 'Sardis', 'Guapos', 'Pieology', 'Boston Market', 'Wow Deli', 'Firehouse Subs', 'Mission BBQ', 'Halal Guys', 'The Habit', 'Potbelly', 'Qdoba', 'Asia Cafe', 'Woodside Deli', 'Gordon Biersch', 'Mellow Mushroom', 'Wing Stop', 'BWW', 'Giuseppi',  'Cooking at Home', 'Taco Bell'   )
+
+group=" ".join(random.choice(food) for _ in range(1))
+
+print(group)
