49ff0b510d9f6f608f9cf0a63378fb21c4246806
kingchris1031 <alan_grant10@yahoo.com>
2019-09-20 11:25:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 
-food=('Burger King','Five Guys','BGR Joint',' Nandos', 'Checkers', 'New Fortune', 'Subway', 'Wendys', 'Baja Fresh', 'Jerrys', 'Corner Bakery', 'Red Robin', 'McDonalds', 'KFC', 'Panera',' Taco Bar', 'IHop(IHob)', 'Popeyes', 'Red Lobster', 'Chipotle', 'Sardis', 'Guapos', 'Pieology', 'Boston Market', 'Wow Deli', 'Firehouse Subs', 'Mission BBQ', 'Halal Guys', 'The Habit', 'Potbelly', 'Qdoba', 'Asia Cafe', 'Woodside Deli', 'Gordon Biersch', 'Mellow Mushroom', 'Wing Stop', 'BWW', 'Giuseppi',  'Cooking at Home', 'Taco Bell'   )
+food=('Burger King','Five Guys','BGR Joint',' Nandos', 'Checkers', 'New Fortune', 'Subway', 'Wendys', 'Baja Fresh', 'Jerrys', 'Corner Bakery', 'Red Robin', 'McDonalds', 'KFC', 'Panera',' Taco Bar', 'IHop(IHob)', 'Popeyes', 'Red Lobster', 'Chipotle', 'Sardis', 'Guapos', 'Pieology', 'Boston Market', 'Wow Deli', 'Firehouse Subs', 'Mission BBQ', 'Halal Guys', 'The Habit', 'Potbelly', 'Qdoba', 'Asia Cafe', 'Woodside Deli', 'Gordon Biersch', 'Mellow Mushroom', 'Wing Stop', 'BWW', 'Giuseppi',  'Cooking at Home', 'Taco Bell', 'First Place You Pass' )
 
 group=" ".join(random.choice(food) for _ in range(1))
 
